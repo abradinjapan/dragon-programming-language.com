47735b41ac676d58bf835aace6d4d362fde50890
abradinjapan <abradinjapan@outlook.com>
2024-12-30 17:58:39 -0500

    Added built in types documentation.

--- scripting.js
+++ scripting.js
@@ -112,6 +112,104 @@ let site_json = {
                     data: "foo(a, b, c, d)()"
                 }
             ]
+        },
+        {
+            name: "documentation.home",
+            top_links: "normal",
+            left_links: "documentation",
+            content: [
+                {
+                    type: "header",
+                    data: "Documentation Home"
+                },
+                {
+                    type: "text",
+                    data: "Dragon's built in structures and functions."
+                },
+                {
+                    type: "text",
+                    data: "Search by selecting a link on the left side of the webpage."
+                }
+            ]
+        },
+        {
+            name: "documentation.structures",
+            top_links: "normal",
+            left_links: "documentation",
+            content: [
+                {
+                    type: "header",
+                    data: "Dragon Built In Structures"
+                },
+                {
+                    type: "text",
+                    data: "All built in structures."
+                },
+                {
+                    type: "header",
+                    data: "!dragon.cell"
+                },
+                {
+                    type: "code_block",
+                    data: "!dragon.cell\n(\n\t[internal_value]\n)"
+                },
+                {
+                    type: "text",
+                    data: "The dragon cell is the single foundational structure type in Dragon. It cannot be divided further into smaller variables. It's usage is like a register in assembly. That means that every !dragon.cell is a fixed size of 64 bits and how you operate on it depends on what the data inside the cell is intended to be. There is no separate integer type, pointer type, floating point type, boolean type, etc. Just cells and structures."
+                },
+                {
+                    type: "header",
+                    data: "!dragon.buffer"
+                },
+                {
+                    type: "code_block",
+                    data: "!dragon.buffer\n(\n\tstart !dragon.cell,\n\tend !dragon.cell\n)"
+                },
+                {
+                    type: "text",
+                    data: "The dragon buffer is the universal type for dealing with any kind of range of memory from its starting byte to its ending byte. Hence the 'start' member and the 'end' member."
+                },
+                {
+                    type: "header",
+                    data: "!dragon.current"
+                },
+                {
+                    type: "code_block",
+                    data: "!dragon.current\n(\n\tbuffer !dragon.buffer,\n\tprogress !dragon.cell\n)"
+                },
+                {
+                    type: "text",
+                    data: "The dragon current is a type dedicated to walking through buffers using pointers. The 'buffer' member is the range of the walk and the 'progress' is the current byte ready to be read.",
+                },
+                {
+                    type: "header",
+                    data: "!dragon.list"
+                },
+                {
+                    type: "code_block",
+                    data: "!dragon.list\n(\n\tallocation !dragon.buffer,\n\tlength !dragon.cell,\n\tincrease !dragon.cell,\n\tappend_count !dragon.cell\n)"
+                },
+                {
+                    type: "text",
+                    data: "The dragon list is a type of buffer that expands as more memory is required. The allocation is the physical block of memory that holds the list. The length holds the amount of bytes that the list is currently using. When the allocation needs to be expanded, a new buffer equivalent to the old allocation plus 'increase' is allocated. After the allocation the old buffer has it's data copied to the new allocation, the old buffer is deallocated and is replaced."
+                },
+                {
+                    type: "text",
+                    data: "IMPORTANT: Keep in mind that when the list has to reallocate and copy to expand it's capacity, the old buffer is deallocated and the new one takes it's place. Be careful not to setup a pointer to a list that might need to be reallocated!"
+                },
+                {
+                    type: "header",
+                    data: "!dragon.time"
+                },
+                {
+                    type: "code_block",
+                    data: "!dragon.time\n(\n\tseconds !dragon.cell,\n\tnanoseconds !dragon.cell\n)"
+                },
+                {
+                    type: "text",
+                    data: "The dragon time structure is an integer representation of seconds and nanoseconds. This structure captures a point in time from a dragon function and can be subtracted and added to measure program execution time."
+                }
+            ]
         }
     ],
     top_links: [
@@ -125,6 +223,10 @@ let site_json = {
                 {
                     text: "Tutorial",
                     page: "tutorial.home",
+                },
+                {
+                    text: "Documentation",
+                    page: "documentation.home"
                 },
                 {
                     text: "Links",
@@ -144,6 +246,19 @@ let site_json = {
                 {
                     text: "Welcome!",
                     page: "tutorial.home",
+                }
+            ]
+        },
+        {
+            name: "documentation",
+            content: [
+                {
+                    text: "Home",
+                    page: "documentation.home"
+                },
+                {
+                    text: "Built In Structures",
+                    page: "documentation.structures"
                 }
             ]
         }
